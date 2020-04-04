6d90f022881e739ca7578bacb8666cbc0968a8dc
zymfe <1047832475@qq.com>
2020-04-05 00:23:30 +0800

    re-build

--- dist/5.b454cd71a4049922cd20.js
+++
@@ -1,2 +0,0 @@
-/*! created 2019/04/29 by zhaoyiming */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{145:function(n,e,t){"use strict";t.r(e),t(80);var a=t(90),o=t.n(a),r=(t(78),t(79)),i=t.n(r),c=t(0),s=t.n(c),u=t(1);e.default=function(){var n=Object(u.useHistory)();return s.a.createElement("div",null,s.a.createElement("h2",null,"Home"),s.a.createElement(i.a,{type:"primary",onClick:function(){n.push("/dashboard")}},"to dashboard"),s.a.createElement(i.a,{type:"warning",onClick:function(){return o.a.info("this is a Toast component")}},"show toast"))}}}]);
