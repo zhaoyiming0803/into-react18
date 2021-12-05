39e2e84f5e19cc872b498d03d4196958cc64637c
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-12-05 18:52:06 +0800

    test: treen data struct and change value

---
+++ src/components/Tree.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import { useState } from 'react'
+import { deepCopy } from '../utils/tools'
+
+interface CityItem {
+  name: string
+  id: number
+  pid: number
+  level: number,
+  children?: CityItem[]
+}
+
+interface Props {
+  key: number
+  index: number
+  path: number[]
+  cityData: CityItem,
+  citys: CityItem[],
+  onChange (path: number[], value: string): any,
+  setCitys (citys: CityItem[]): any
+}
+
+export default function Tree<T extends Props> (props: T) {
+  const [cityData, setCityName] = useState(props.cityData)
+  const path = props.path.concat(props.index)
+  return (
+    <div>
+      <div style={{marginLeft: 20 * cityData.level + 'px'}}>
+        <input type="text" value={ cityData.name } onChange={e => {
+          setCityName({
+            ...cityData,
+            name: e.target.value
+          })
+          props.onChange(path, e.target.value)
+        }} />
+      </div>
+      {
+        Array.isArray(cityData.children) && cityData.children.map((city, index) => <Tree citys={props.citys} cityData={city} key={index} index={index} path={path} setCitys={props.setCitys} onChange={(path: number[], value: string) => {
+          const _citys = deepCopy(props.citys)
+          const target = path.reduce((target, p, index) => {
+            if (index === path.length - 1) {
+              return target[p]
+            } else {
+              return target[p].children
+            }
+          }, _citys)
+          target.name = value
+          props.setCitys(_citys)
+        }}></Tree>)
+      }
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,16 +6,50 @@ import { User } from "@/types/index";
 import { RootState } from "@/reducer/index";
 import { addUser } from "@/action/index";
 import { Button } from "antd-mobile";
+import Tree from '../components/Tree'
+import { deepCopy } from '../utils/tools'
 
 interface Props {
   userList: User[];
   addUser: (user: User) => void;
 }
 
+const cityList = [
+  { name: '北京市', id: 1, pid: 0, level: 0 },
+  { name: '朝阳区', id: 2, pid: 1, level: 1 },
+  { name: '将台', id: 3, pid: 2, level: 2 },
+  { name: '望京', id: 4, pid: 2, level: 2 },
+  { name: '山西省', id: 5, pid: 0, level: 0 },
+  { name: '太原市', id: 6, pid: 5, level: 1 }
+]
+
+interface CityItem {
+  name: string
+  id: number
+  pid: number
+  level: number,
+  children?: CityItem[]
+}
+
+function formatCityList (cityList: CityItem[], pid = 0): CityItem[] {
+  let res: CityItem[] = []
+  cityList.forEach(city => {
+    if (city.pid === pid) {
+      const children = formatCityList(cityList, city.id)
+      if (children.length) {
+        city.children = children
+      }
+      res.push(city)
+    }
+  })
+  return res
+}
+
 function Home<T extends Props>(props: T) {
   console.log(props);
   const [count, setCount] = useState(0);
   const hisgory = useHistory();
+  const [citys, setCitys] = useState(formatCityList(cityList))
 
   return (
     <div>
@@ -39,6 +73,22 @@ function Home<T extends Props>(props: T) {
       <Button onClick={() => hisgory.push("/handleValueByHooks")}>
         to handle value by hooks page
       </Button>
+      <div>
+        {citys.map((city, index) => <Tree citys={citys} cityData={city} key={index} index={index} path={[]} setCitys={setCitys} onChange={(path: number[], value: string) => {
+          const _citys = deepCopy(citys)
+          let target = _citys
+          path.forEach((p, i) => {
+            if (i === path.length - 1) {
+              target = target[p]
+            } else {
+              target = target[p].children
+            }
+          })
+          target.name = value
+          setCitys(_citys)
+        }}></Tree>)}
+      </div>
+      <div>stringifyTreeData: { JSON.stringify(citys) }</div>
     </div>
   );
 }
