962e00ed3ac95753fc7814e56af9dc8c4e5dcba8
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-04-02 18:41:36 +0800

    feat: upgrade to react 18

--- index.tsx
+++ index.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "@/App";
 import store from "@/store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+const root = createRoot(document.getElementById("root"))
+
+root.render(<Provider store={store}>
+  <App />
+</Provider>)
