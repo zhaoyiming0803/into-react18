aa9f5599997a31edb8a8ff826eb1e733984024e8
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-04-08 20:09:59 +0800

    test batching update in react18

--- src/pages/TestReact18.tsx
+++ src/pages/TestReact18.tsx
@@ -17,32 +17,48 @@ export default function TestReact18<T extends Props> (props: T) {
 
   const addCountAsync = () => {
     for (let i = 1; i <= 3; i++) {
-      setCount(count + 1)
+      setCount(count + i)
+      // 无法实时获取到 count 最新值
+      // console.log('TestReact18 render') 执行了 1 次，说明 React 18 在非受控场景下也是自动批处理
+      console.log('addCountAsync count: ', count)
     }
-    // 无法实时获取到 count 最新值
-    //  useEffect 中的回调执行了 1 次，说明默认是自动批处理
-    console.log('addCountAsync count: ', count)
   }
 
   const addCountFlushSync = () => {
     // opt-out automatic batching
     for (let i = 1; i <= 3; i++) {
       flushSync(() => {
-        setCount(count + 1)
+        setCount(count + i)
         // 无法实时获取到 count 最新值
-        // useEffect 中的回调执行了 3 次，说明没有批处理
+        // console.log('TestReact18 render') 执行了 3 次，说明 React 18 在非受控场景下也是自动批处理
+        // 相同的，useEffect count: 也打印了 3 次
         console.log('addCountFlushSync count: ', count)
       })
     }
   }
 
-  const addCountInCallback = () => {
-    setCount((count) => {
-      count += 1
-      return count
+  const addCountInSetTimeout = () => {
+    // opt-out automatic batching
+    setTimeout(() => {
+      for (let i = 1; i <= 3; i++) {
+        setCount(count + i)
+        // 无法实时获取到 count 最新值
+        // console.log('TestReact18 render') 执行了 1 次，说明 React 18 在非受控场景下也是自动批处理
+        console.log('addCountInSetTimeout count: ', count)
+      }
     })
-    // 无法实时获取到 count 最新值
-    console.log('setCount callback: ', count)
+  }
+
+  const addCountInCallback = () => {
+    for (let i = 1; i <= 3; i++) {
+      setCount((count: number) => {
+        count += i
+        return count
+      })
+      // 无法实时获取到 count 最新值
+      // // console.log('TestReact18 render') 执行了 1 次，说明 React 18 在 hooks 回调中也是自动批处理
+      console.log('setCount callback: ', count)
+    }
   }
 
   const getRefDOM = () => {
@@ -69,10 +85,13 @@ export default function TestReact18<T extends Props> (props: T) {
     countCopiedRef.current.innerHTML += `<div style="color: ${color}">${countRef.current.cloneNode(true).innerHTML}</div>`
   }
 
+  console.log('TestReact18 render')
+
   return <div ref={containerRef}>
     <button onClick={addCountAsync}>click async</button>
     <button onClick={addCountFlushSync}>click flushSync</button>
     <button onClick={addCountInCallback}>click callback</button>
+    <button onClick={addCountInSetTimeout}>click in setTimeout</button>
     <button onClick={getRefDOM}>get countRef dom</button>
     <button onClick={insertRefDOM}>insert countRef DOM</button>
     <div ref={countRef}>count: {count}</div>
