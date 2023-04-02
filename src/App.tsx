1834895d462b6308f9c211457137d430eccfd37b
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-04-02 13:05:54 +0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { createElement } from 'react'
+
+import { createRoot, Root } from 'react-dom/client'
+
+function App () {
+  return <>App</>
+}
+
+const root1: Root = createRoot(document.querySelector('#root1'))
+root1.render(<App />)
+
+const root2: Root = createRoot(document.querySelector('#root2'))
+root2.render(createElement('div', {
+  onClick: () => console.log('Like')
+}, 'Like'))
+
