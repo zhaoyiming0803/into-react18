96c72ef6bb5f4735ca7f31c065ca41be8f52c2df
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-04-29 21:17:21 +0800

    test: debugger createRoot

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ function App () {
   return <>App</>
 }
 
+debugger
 const root1: Root = createRoot(document.querySelector('#root1'))
 root1.render(<App />)
 
-const root2: Root = createRoot(document.querySelector('#root2'))
-root2.render(createElement('div', {
-  onClick: () => console.log('Like')
-}, 'Like'))
+// const root2: Root = createRoot(document.querySelector('#root2'))
+// root2.render(createElement('div', {
+//   onClick: () => console.log('Like')
+// }, 'Like'))
 
