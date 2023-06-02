64586e6626206719713a843aecdd0cd992af63c2
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-06-02 16:08:32 +0800

    test: beginWork and completeWork

--- src/App2.tsx
+++ src/App2.tsx
@@ -4,11 +4,20 @@ import { createRoot, Root } from 'react-dom/client'
 
 function App () {
   const [count, setCount] = useState<number>(0)
-  
-  return <button onClick={() => {
-    debugger
-    setCount(count + 1)
-  }}>{count}</button>
+  // React 深度优先遍历
+  // p 标签没有 child 了，然后找 p 的 sibling 即 button
+  // beginWork 和 completeWork 皆是如此
+
+  // RootFiber(beginWork) -> App(beginWork) -> p(beginWork) -> p(completeWork) 
+  // -> p.sibling - button(beginWork) -> button(completeWork)
+  // -> App(completeWork) -> RootFiber(completeWork)
+  return <>
+    <p>123</p>
+    <button onClick={() => {
+      debugger
+      setCount(count + 1)
+    }}>{count}</button>
+  </>
 }
 
 const root1: Root = createRoot(document.querySelector('#root1'))
