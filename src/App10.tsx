64eabcf025526ed122c9d32b8fec29da263f8575
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-06-11 18:35:31 +0800

    test: debugger useState hook in setTimeout

---
+++ src/App10.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+import { createRoot, Root } from 'react-dom/client'
+
+function App () {
+  const [count, setCount] = useState<number>(0)
+
+  const onClickCount = () => {
+    setCount(count + 1)
+
+    // 这里的 count 依旧是 0，而 innerHTML 却是 1 的原因：
+    // count 相当于局部变量
+    // innerHTML 相当于全局变量
+    // https://github.com/zhaoyiming0803/test-code/blob/master/test350.html
+    setTimeout(() => {
+      console.log('count in setTimeout: ', count, document.querySelector('#box').innerHTML) // 0 1
+    }, 1000)
+
+    // 内存中的 count 和 dom 中的 count 都不会实时更新，不是最新值
+    console.log('count in onClickCount: ', count, document.querySelector('#box').innerHTML) // 0 0
+  }
+
+  // renderWithHook
+  useEffect(() => {
+    debugger
+    console.log('count in useEffect: ', count)
+  }, [count])
+
+  return <button onClick={onClickCount} id="box">{count}</button>
+}
+
+const root1: Root = createRoot(document.querySelector('#root1'))
+
+root1.render(<App />)
+
+console.log(root1)
